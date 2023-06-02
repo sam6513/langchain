4d9f56896e131cc87e24ab53df80de4ce98b9516
sam6513 <86785526+sam6513@users.noreply.github.com>
2023-06-02 15:16:42 +0800

    Update generate_prompt.py
    Chinese question and answer

--- langchain/evaluation/qa/generate_prompt.py
+++ langchain/evaluation/qa/generate_prompt.py
@@ -3,7 +3,7 @@ from langchain.output_parsers.regex import RegexParser
 from langchain.prompts import PromptTemplate
 
 template = """You are a teacher coming up with questions to ask on a quiz. 
-Given the following document, please generate a question and answer based on that document.
+Given the following document, please generate a Chinese question and answer based on that document.
 
 Example Format:
 <Begin Document>
